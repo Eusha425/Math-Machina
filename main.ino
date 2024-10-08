15d620d14b6771cef31015c9b776cc2e3d4297d5
Gazi Eusha <52084779+Eusha425@users.noreply.github.com>
2024-10-08 21:03:40 +1100

    added helper functions

--- main.ino
+++ main.ino
@@ -30,11 +30,31 @@ void loop() {
 
     String secondNum = Serial.readStringUntil('\n');
     secondNum.trim(); 
-    
+    inputParse(firstNum);
     // Print the captured values
     Serial.println(firstNum);
     Serial.println(operation);
     Serial.println(secondNum);
     Serial.println("Enter First num: ");
+  }
+}
+
+void inputParse(String input){
+  for(int i = 0; i <input.length(); i++){
+    char ch = input.charAt(i);
+    operation(ch);
+  }
+}
+
+void operation(char ch){
+  if (ch == '1'){
+    // default position
+  BraccioRobot.moveToPosition(pos.set(90,  165, 0, 0, 180,  50), 50);
+  delay(1000);
+  // start movement towards 1
+  BraccioRobot.moveToPosition(pos.set(50,  165, 0, 0, 180,  50), 50);
+  delay(1000);
+  BraccioRobot.moveToPosition(pos.set(50,  105, 0, 0, 180,  50), 50);
+  delay(1000);
   }
 }
